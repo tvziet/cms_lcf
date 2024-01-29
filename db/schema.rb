ab22e8b4bd646c95391eef15ce24fdfd96b8d318
Viet Le <vietleuit@gmail.com>
2024-01-29 20:12:18 +0700

    feat/CMS_06: add document level to admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_28_133736) do
+ActiveRecord::Schema.define(version: 2024_01_28_152837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2024_01_28_133736) do
   end
 
   create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "document_levels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
