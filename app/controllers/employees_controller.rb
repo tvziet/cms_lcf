4aea5b01c8c7b4d2e01e1b6f2185217bae4f670d
Viet Le <vietleuit@gmail.com>
2024-04-02 07:50:44 +0700

    Add colleague info to employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,4 +4,16 @@ class EmployeesController < ApplicationController
   def personal_info; end
 
   def contract_info; end
+
+  def colleague_info
+    company_ids = current_employee.company_ids
+    companies = Company.where(id: company_ids)
+    @colleagues = []
+    companies.find_each do |company|
+      @colleagues << company.employees
+    end
+
+    @colleagues.flatten!
+    @colleagues.delete(current_employee)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'employees/work_info'
   get 'employees/personal_info'
   get 'employees/contract_info'
+  get 'employees/colleague_info'
   get 'pages/home'
   devise_for :administrators
   devise_for :employees
