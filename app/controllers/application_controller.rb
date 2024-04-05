79a64f4d4f44fa5d5ee306e8e4bec084cfe01eb0
Viet Le <vietleuit@gmail.com>
2024-04-05 07:21:03 +0700

    Redirect the employee to root path after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_employee!
+
+  protected
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
 end
