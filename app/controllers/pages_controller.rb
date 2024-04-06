1e8d50baab8bf22581f1cfb22dab1d6eccab1241
Viet Le <vietleuit@gmail.com>
2024-04-06 10:05:52 +0700

    Update home page content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,7 @@
 class PagesController < ApplicationController
   def home
     @carousels = Carousel.all
-    @mine_documents = Document.where(employee_id: current_employee.id)
-    @my_company_documents = Document.where(company_id: current_employee.company_ids)
-    @my_group_documents = Document.where(group_ids: current_employee.group_ids)
+    @news = News.public_news.limit(10)
+    @documents = Document.public_documents.limit(10)
   end
 end
